28b0ce21c9a7b05bb501c133092b3961b60f4e0b
saulperez37 <sperez_44@hotmail.com>
2019-09-02 22:44:02 -0700

    updated javascript

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,5 @@
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+    "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 let wins = 0;
 let losses = 0;
@@ -10,21 +11,31 @@ let lossesText = document.getElementById("losses");
 let guessesLeftText = document.getElementById("guesses-left");
 let guessesText = document.getElementById("your-guesses");
 
+let restart = function () {
+    guessesLeft = 10;
+    guesses = [];
+    let computerGuess = letters[Math.floor(Math.random() * letters.length)];
+}
+
 document.onkeyup = function (event) {
     let userGuess = event.key;
+    guesses.push(userGuess);
+    guessesLeft--;
 
     let computerGuess = letters[Math.floor(Math.random() * letters.length)];
 
     if (userGuess === computerGuess) {
         wins++;
-    } else {
+        restart();
+    } else if (guessesLeft < 1) {
         losses++;
+        restart();
     }
 
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
-    guessesText.textContent = "Your guesses so far: " + userGuess;
-
+    guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    guessesText.textContent = "Your guesses so far: " + guesses;
 
 }
 
